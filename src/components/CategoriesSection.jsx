bdb95644ffe057e32a5d4253ef36ee84a8e04b51
ABigJuicyBurger <bader_muhssin@hotmail.com>
2025-12-09 13:31:47 -0700

    start categories section

--- src/components/CategoriesSection.jsx
+++ src/components/CategoriesSection.jsx
@@ -1,12 +1,23 @@
 import { useState } from "react";
 import { TopicsList } from "../TopicsList";
+import styled from "styled-components";
 
 export function CategoriesSection() {
   const [topicsList, setTopicsList] = useState(["Coding", "Finance", "Life"]);
   return (
-    <section className="categoriesSection">
-      <h2>Categories</h2>
-      <TopicsList topics={topicsList} />
-    </section>
+    <StyledSection>
+      <section className="categoriesSection">
+        <h2>Categories</h2>
+        <TopicsList topics={topicsList} />
+      </section>
+    </StyledSection>
   );
 }
+
+const StyledSection = styled.section`
+  h2 {
+    color: papayawhip;
+  }
+  display: flex;
+  flex-direction: column;
+`;
